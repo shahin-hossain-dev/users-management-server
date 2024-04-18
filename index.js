9c5ce06f643a1312fa991bf1fab0a270437480ba
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-18 22:10:13 +0600

    54-6 (Recap) Install Node-Express And Use Fetch To Load Data

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// এখানে process.env.PORT use করার কারণ হলো যখন আমরা ‍ server Deploy করবো, যেখানে deploy করবো সেখানে যে Available Port আছে সেখান থেকে Access করবে।
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+const users = [
+  { id: 1, name: "Shahin", email: "shain@gmail.com" },
+  { id: 2, name: "mim", email: "mim@gmail.com" },
+  { id: 3, name: "jui", email: "jui@gmail.com" },
+  { id: 4, name: "akbor", email: "akbor@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("User Management server is Running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running port on ${port}`);
+});
